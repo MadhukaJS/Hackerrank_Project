237c4c8b1303c781998ef3948d030603e309dd73
Madhuka Priyashan <madhuka164@gmail.com>
2023-03-03 03:33:31 +0530

    ifelse fixed

--- HackerrankBasic/src/ifElse.java
+++ HackerrankBasic/src/ifElse.java
@@ -29,14 +29,14 @@ public class ifElse {
             System.out.println("Weird");
         }
         else {
-            if(n>2 && n<5){
+            if(n>2 && n<=5){
                 System.out.println("Not Weird");
 
             }
-            else if(n>6 && n<20){
+            else if(n>6 && n<=20){
                 System.out.println("Weird");
             }
-            else if(n>20){
+            else{
                 System.out.println("Not Weird");
 
             }
